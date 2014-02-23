9d68c142705fe5cf0f24d542d9bd2122b1f19c8b
shalin94 <shalin94@gmail.com>
2014-02-22 19:44:05 -0500

    All Activities and classes created

--- Android/gen/com/cs307/boilerlab/R.java
+++ Android/gen/com/cs307/boilerlab/R.java
@@ -31,15 +31,27 @@ public final class R {
         public static final int button4=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_lab_list=0x7f030000;
+        public static final int activity_lab_view=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_map=0x7f030003;
+        public static final int activity_preferences=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int lab_list=0x7f070000;
+        public static final int lab_view=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int map=0x7f070003;
+        public static final int preferences=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_lab_list=0x7f050005;
+        public static final int title_activity_lab_view=0x7f050003;
+        public static final int title_activity_map=0x7f050004;
+        public static final int title_activity_preferences=0x7f050006;
     }
     public static final class style {
         /** 
