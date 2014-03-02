27203ff82c0681bfeef4316c1ea4b27c000563cf
gupta153@purdue.edu <agupta94.ag@gmail.com>
2014-03-02 14:41:44 -0500

    buttons updated

--- Android/src/com/cs307/boilerlab/MainActivity.java
+++ Android/src/com/cs307/boilerlab/MainActivity.java
@@ -2,7 +2,10 @@ package com.cs307.boilerlab;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button map = (Button) findViewById (R.id.buttonmap);
+		Button closest = (Button) findViewById (R.id.buttonclosest);
+		Button listlab = (Button) findViewById (R.id.buttonlist);
+		Button preferences = (Button) findViewById (R.id.buttonpref);
+		map.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent map = new Intent(MainActivity.this,Map.class);
+				MainActivity.this.startActivity(map);
+				// TODO Auto-generated method stub
+			}
+		});
+		
+		closest.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent closest = new Intent(MainActivity.this,LabView.class);
+				MainActivity.this.startActivity(closest);
+				// TODO Auto-generated method stub
+			}
+		});
 	}
 
 	@Override
