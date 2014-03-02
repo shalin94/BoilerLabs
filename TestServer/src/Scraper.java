af5ce10d42a4723ed8bb2b26e3ef72a3452bc200
shalin94 <shalin94@gmail.com>
2014-03-02 16:31:49 -0500

    Created a method to get data for specific labs

--- TestServer/src/Scraper.java
+++ TestServer/src/Scraper.java
@@ -1,5 +1,7 @@
 
 
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.safety.*;
 import org.jsoup.nodes.*;
@@ -9,15 +11,38 @@ import org.jsoup.select.Elements;
 
 
 public class Scraper {
+	public ArrayList<String> getData(String building,String room){
+		String url = "https://lslab.ics.purdue.edu/icsWeb/LabInfo?building=";
+		url = url + building;
+		url = url + "&room=" + room;
+		ArrayList<String> data = new ArrayList<String>();
+		try{
+			Document document = Jsoup.connect(url).get();
+			Elements stuff = document.select("span.site_md");
+			for (Element a : stuff) {
+				System.out.println("\n" + a.text().replace("\u00a0", "") );
+				data.add(a.text().replace("\u00a0", ""));
+				
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+		return data;
+		
+	}
 	public static void main(String[] args){
-		String url = "https://lslab.ics.purdue.edu/icsWeb/LabInfo?building=GRIS&room=121";
+		String url = "https://lslab.ics.purdue.edu/icsWeb/LabInfo?building=BCC&room=204";
 		System.out.println("Fetching " + url);
+		ArrayList<String> String = new ArrayList<String>();
 		try {
 			Document document = Jsoup.connect(url).get();
 			System.out.println("Trying....");
 			Elements stuff = document.select("span.site_md");
 			for (Element a : stuff) {
 				System.out.println("\n" + a.text().replace("\u00a0", "") );
+				String.add(a.text().replace("\u00a0", ""));
+				
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
