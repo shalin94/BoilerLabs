498a391360cecacd8aff84dd421a9635d5cc5422
Ayan <ayan1294@gmail.com>
2014-03-07 22:25:49 -0500

    GPS User location.

--- Android/src/com/cs307/boilerlab/Map.java
+++ Android/src/com/cs307/boilerlab/Map.java
@@ -1,21 +1,77 @@
 package com.cs307.boilerlab;
 
+import java.util.List;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+//import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class Map extends MapActivity {
+public class Map extends Activity {
 
+	private double[] getGPS() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
+        List<String> providers = lm.getProviders(true);
+
+        /* Loop over the array backwards, and if you get an accurate location, then break out the loop*/
+        Location l = null;
+        
+        for (int i=providers.size()-1; i>=0; i--) {
+                l = lm.getLastKnownLocation(providers.get(i));
+                if (l != null) break;
+        }
+        
+        double[] gps = new double[2];
+        if (l != null) {
+                gps[0] = l.getLatitude();
+                gps[1] = l.getLongitude();
+        }
+        return gps;
+}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
+		setContentView(R.layout.activity_map);	
 		
-        
-		/*MapView mapView = (MapView) findViewById(R.id.mapView);
-	    mapView.setBuiltInZoomControls(true); */
+		double[] g=getGPS();
+		
+		
+		// Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(g[0], g[1]);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        /*map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));*/
 	}
 
 	@Override
@@ -24,11 +80,6 @@ public class Map extends MapActivity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	
 
 }
