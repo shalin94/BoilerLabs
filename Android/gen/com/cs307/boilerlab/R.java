2b878155ec1d384864774c3e76811cba6d83bfcc
rahluwal <rahluwal@purdue.edu>
2014-03-31 14:20:22 -0400

    Added mini map and improved lists. Lists incomplete as of now

--- Android/gen/com/cs307/boilerlab/R.java
+++ Android/gen/com/cs307/boilerlab/R.java
@@ -230,22 +230,23 @@ containing a value of this type.
         public static final int Sun=0x7f05000c;
         public static final int SunTime=0x7f05000d;
         public static final int Type=0x7f050006;
-        public static final int action_settings=0x7f050018;
-        public static final int badge=0x7f050013;
-        public static final int buttonclosest=0x7f050010;
-        public static final int buttonlist=0x7f05000e;
-        public static final int buttonmap=0x7f050011;
-        public static final int buttonpref=0x7f05000f;
+        public static final int action_settings=0x7f050019;
+        public static final int badge=0x7f050014;
+        public static final int buttonclosest=0x7f050011;
+        public static final int buttonlist=0x7f05000f;
+        public static final int buttonmap=0x7f050012;
+        public static final int buttonpref=0x7f050010;
         public static final int hybrid=0x7f050004;
-        public static final int inputSearch=0x7f050016;
-        public static final int listview=0x7f050017;
-        public static final int map=0x7f050012;
+        public static final int inputSearch=0x7f050017;
+        public static final int listview=0x7f050018;
+        public static final int map=0x7f050013;
+        public static final int map2=0x7f05000e;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
-        public static final int snippet=0x7f050015;
+        public static final int snippet=0x7f050016;
         public static final int terrain=0x7f050003;
-        public static final int title=0x7f050014;
+        public static final int title=0x7f050015;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
--- Android/src/com/cs307/boilerlab/LabView.java
+++ Android/src/com/cs307/boilerlab/LabView.java
@@ -1,20 +1,68 @@
 package com.cs307.boilerlab;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import org.w3c.dom.Document;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LabView extends Activity {
 
+public class LabView extends Activity {
+	
+	private double[] getGPS() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
+        List<String> providers = lm.getProviders(true);
+
+        /* Loop over the array backwards, and if you get an accurate location, then break out the loop*/
+        Location l = null;
+        
+        for (int i=providers.size()-1; i>=0; i--) {
+                l = lm.getLastKnownLocation(providers.get(i));
+                if (l != null) break;
+        }
+        
+        double[] gps = new double[2];
+        if (l != null) {
+                gps[0] = l.getLatitude();
+                gps[1] = l.getLongitude();
+        }
+        return gps;
+}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lab_view);
+		
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+		
 		Intent i = getIntent();
 		String name = i.getStringExtra("name");
 		TextView txtName = (TextView) findViewById(R.id.Name);
@@ -52,6 +100,67 @@ public class LabView extends Activity {
 		} finally {
 			myDbHelper.close();
 		}
+		
+		//The Mini Map Stuff
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map2)).getMap();
+		map.setMyLocationEnabled(true);
+		map.setPadding(0, 0, 0, 0);
+		double[] g=getGPS();
+		//g[0]=(double)Math.round(g[0] * 1000000) / 1000000;
+		//g[1]=(double)Math.round(g[1] * 1000000) / 1000000;
+		LatLng sydney = new LatLng(g[0], g[1]);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
+		String [] names = name.split(" ");
+		String name3=names[0];
+		//name3=name3.trim();
+		LatLng finloc=null;
+        try{
+			myDbHelper = new DatabaseHelper(LabView.this);
+			List<Buildings> bldg = myDbHelper.getBuilding();
+			Iterator<Buildings> it = bldg.iterator();
+			while(it.hasNext()){
+				Buildings temp = it.next();
+				String name2 = temp.getName();
+				name2=name2.trim();
+				Log.d("TESTTT","I am here with !"+name2+"!");
+				Log.d("TESTTT","---and with !"+name3+"!");
+				if(!name2.equals(name3))
+				{
+					continue;
+				}
+				//Log.d("...","I am here Too");
+				String loc = temp.getBuildingLoc();
+				String [] locs = loc.split(",");
+				Log.d("directions","1: "+locs[0]+" 2: "+locs[1]);
+				Log.d("Sydney directions","1: "+sydney.latitude+" 2: "+sydney.longitude);
+				finloc=new LatLng(Double.parseDouble(locs[0]), Double.parseDouble(locs[1]));
+				map.addMarker(new MarkerOptions()
+		        .position(finloc)
+		        .title(name));
+				
+				MapDirection md = new MapDirection();
+
+		        Document doc = md.getDocument(sydney, finloc, MapDirection.MODE_DRIVING);
+		        
+		        ArrayList<LatLng> directionPoint = md.getDirection(doc);
+
+		        PolylineOptions rectLine = new PolylineOptions().width(5).color(Color.argb(255, 51, 181, 229)).geodesic(true);
+		        
+
+		        for(int i1 = 0 ; i1 < directionPoint.size() ; i1++) {          
+		        rectLine.add(directionPoint.get(i1));
+		        }
+		        map.addPolyline(rectLine);
+		        
+				break;
+			}
+		}catch(Exception e){
+			Log.e(this.getClass().getName(), "Failed to run query", e);
+		} finally {
+			myDbHelper.close();
+		}
+        
+ 
 		
 	}
 
